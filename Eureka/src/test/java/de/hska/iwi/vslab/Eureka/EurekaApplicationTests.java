23b3686606bbf4e7ea8f9065385bcb41bf4f42d8
and1kl <40032530+and1kl@users.noreply.github.com>
2019-11-18 20:12:47 +0100

    Initial Commit

---
+++ Eureka/src/test/java/de/hska/iwi/vslab/Eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
